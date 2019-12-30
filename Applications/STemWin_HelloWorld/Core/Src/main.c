24e540cc64d32270b5bfd245a98ec8a974936afe
xpress-embedo <laboratory.embedded@gmail.com>
2019-12-30 18:49:15 +0100

    * Different Types of Text Display

--- Applications/STemWin_HelloWorld/Core/Src/main.c
+++ Applications/STemWin_HelloWorld/Core/Src/main.c
@@ -6,37 +6,37 @@
   ******************************************************************************
   * @attention
   *
-  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics International N.V. 
+  * <h2><center>&copy; Copyright (c) 2016 STMicroelectronics International N.V.
   * All rights reserved.</center></h2>
   *
-  * Redistribution and use in source and binary forms, with or without 
+  * Redistribution and use in source and binary forms, with or without
   * modification, are permitted, provided that the following conditions are met:
   *
-  * 1. Redistribution of source code must retain the above copyright notice, 
+  * 1. Redistribution of source code must retain the above copyright notice,
   *    this list of conditions and the following disclaimer.
   * 2. Redistributions in binary form must reproduce the above copyright notice,
   *    this list of conditions and the following disclaimer in the documentation
   *    and/or other materials provided with the distribution.
-  * 3. Neither the name of STMicroelectronics nor the names of other 
-  *    contributors to this software may be used to endorse or promote products 
+  * 3. Neither the name of STMicroelectronics nor the names of other
+  *    contributors to this software may be used to endorse or promote products
   *    derived from this software without specific written permission.
-  * 4. This software, including modifications and/or derivative works of this 
+  * 4. This software, including modifications and/or derivative works of this
   *    software, must execute solely and exclusively on microcontroller or
   *    microprocessor devices manufactured by or for STMicroelectronics.
-  * 5. Redistribution and use of this software other than as permitted under 
-  *    this license is void and will automatically terminate your rights under 
-  *    this license. 
+  * 5. Redistribution and use of this software other than as permitted under
+  *    this license is void and will automatically terminate your rights under
+  *    this license.
   *
-  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
-  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
-  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
   * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
-  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
   * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
   * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
-  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
-  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
   * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
   * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
   *
@@ -65,41 +65,41 @@ extern void MainTask(void);
   * @brief  Main program.
   * @param  None
   * @retval None
-  */ 
-int main(void)
-{  
+  */
+int main( void )
+{
   /* Enable the CPU Cache */
   CPU_CACHE_Enable();
-  
+
   /* STM32F7xx HAL library initialization:
        - Configure the Flash ART accelerator on ITCM interface
        - Configure the Systick to generate an interrupt each 1 msec
        - Set NVIC Group Priority to 4
        - Global MSP (MCU Support Package) initialization
      */
-  HAL_Init();  
-  
+  HAL_Init();
+
   /* Configure the system clock to 200 MHz */
   SystemClock_Config();
-  
+
   /* Init the STemWin GUI Library */
   BSP_SDRAM_Init(); /* Initializes the SDRAM device */
   __HAL_RCC_CRC_CLK_ENABLE(); /* Enable the CRC Module */
-  
+
   GUI_Init();
 
   /* Enable Window Manager Multibuffering */
   WM_MULTIBUF_Enable(1);
-    
+
   MainTask();
-  
+
   /* Infinite loop */
   for(;;);
 }
 
 /**
   * @brief  System Clock Configuration
-  *         The system Clock is configured as follow : 
+  *         The system Clock is configured as follow :
   *            System Clock source            = PLL (HSE)
   *            SYSCLK(Hz)                     = 200000000
   *            HCLK(Hz)                       = 200000000
@@ -130,19 +130,19 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
   RCC_OscInitStruct.PLL.PLLM = 25;
-  RCC_OscInitStruct.PLL.PLLN = 400;  
+  RCC_OscInitStruct.PLL.PLLN = 400;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = 9;
   HAL_RCC_OscConfig(&RCC_OscInitStruct);
-          
+
   ret = HAL_PWREx_EnableOverDrive();
 
   if(ret != HAL_OK)
   {
     while(1) { ; }
-  }  
-  
-  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 
+  }
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
      clocks dividers */
   RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
--- Applications/STemWin_HelloWorld/STemWin/App/BASIC_HelloWorld.c
+++ Applications/STemWin_HelloWorld/STemWin/App/BASIC_HelloWorld.c
@@ -36,8 +36,31 @@ void MainTask(void)
 {
   GUI_Clear();
   GUI_SetFont(&GUI_Font32_1);
-  /*Tutorial 1: Simple Example to display text on the display*/
-  GUI_DispStringAt("Hello World!", (LCD_GetXSize()-100)/2, (LCD_GetYSize()-20)/2);
+  /* Displays the string at the current text position in the current window using the current font.*/
+  GUI_DispString("Hello World\r\n");
+  GUI_DispString("Getting Started with STemWin Library\r\n");
+  /*Change Background Color*/
+  GUI_SetBkColor(GUI_BLUE);
+  /* Displays the string at the specified position in the current window using the current font.*/
+  GUI_DispStringAt("Writing at Specific Position with Blue Background", 0, 64);
+  GUI_DispStringAt("Clearing the complete line", 0, 96);
+  /*Clears the current line in the current window from the current text position
+  to the end of the window using the height of the current font.*/
+  GUI_DispCEOL();
+  /*Combining the above two functions*/
+  GUI_DispStringAtCEOL("Displaying Text and Cleaning Line in a single statement", 0, 128);
+
+  GUI_SetBkColor(GUI_RED);
+  /*Display Text Horizontally Centered*/
+  GUI_DispStringHCenterAt("Horizontal Centered Text", LCD_GetXSize()/2, 160);
+  /*Displaying Text Within Rectangle with Rotation*/
+  GUI_SetFont(&GUI_Font8_1);
+  GUI_RECT Rect = {10, 192, 40, 256 };
+  char acText[] = "Rotated\ntext";
+  GUI_SetTextMode(GUI_TM_XOR);
+  GUI_FillRectEx(&Rect);
+  /*GUI_ROTATE_0, GUI_ROTATE_180, GUI_ROTATE_CCW, GUI_ROTATE_CW*/
+  GUI_DispStringInRectEx(acText, &Rect, GUI_TA_HCENTER | GUI_TA_VCENTER, strlen(acText), GUI_ROTATE_CW );
   while(1);
 }
 
